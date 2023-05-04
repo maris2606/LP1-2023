9417f4ad126f1dd07c68b051900652dc178f047b
Marisa Mayumi Morita Ouchi <126785489+maris2606@users.noreply.github.com>
2023-05-04 11:35:20 -0300

    Add files via upload

---
+++ 2° Bimestre/maris 04 05/src/Roda.java
@@ -0,0 +1,25 @@
+public class Roda {
+    private int aro;
+    private int calibragem;
+
+    public Roda(int aro, int calibragem) {
+        this.aro = aro;
+        this.calibragem = calibragem;
+    }
+
+    public int getAro() {
+        return aro;
+    }
+
+    public void setAro(int aro) {
+        this.aro = aro;
+    }
+
+    public int getCalibragem() {
+        return calibragem;
+    }
+
+    public void setCalibragem(int calibragem) {
+        this.calibragem = calibragem;
+    }
+}
