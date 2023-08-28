c4393e7f0f6d4f82543bd5ef34f4624d6a6c119b
Marisa Mayumi Morita Ouchi <126785489+maris2606@users.noreply.github.com>
2023-08-28 15:57:27 -0300

    Add files via upload

---
+++ 3° Bimestre/Loja 24 08/src/main/java/entity/Pessoa.java
@@ -0,0 +1,82 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="pessoa")
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pessoa")
+    private int id;
+    @Column
+    private String nome;
+    @Column
+    private String sobrenome;
+    @Column
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL) //um para um
+    @JoinColumn(name = "endereco_Id")
+    private Endereco endereco;
+    //private List<> pedido
+    //@ManyToOne
+
+    public Pessoa(String nome, String sobrenome, String email, Endereco endereco) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", email='" + email + '\'' +
+                ", endereco=" + endereco +
+                '}';
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
